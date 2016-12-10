ee2ee85316f604b7b4473258c660eb4bf5681634
Domagoj Štrekelj <domagojstrekelj@gmail.com>
2016-12-10 14:36:40 +0100

    Add loader query utility

--- cfg/webpack.config.js
+++ cfg/webpack.config.js
@@ -2,43 +2,76 @@ var path = require('path')
 
 var BASE = path.resolve(__dirname, '..')
 
+function query(loader, parameters) {
+  return loader + '?' + JSON.stringify(parameters)
+}
+
 module.exports = {
   entry: path.join(BASE, 'src', 'index.js'),
   loaders: {
     image: {
       test: /\.(jpe?g|png|gif|svg)$/i,
       loaders: [
-          'file-loader?hash=sha512&digest=hex&name=[hash].[ext]',
-          'image-webpack-loader?bypassOnDebug&optimizationLevel=7&interlaced=false'
+        query(
+          'file-loader',
+          {
+            digest: 'hex',
+            hash: 'sha512',
+            name: '[hash].[ext]',
+          }
+        ),
+        query(
+          'image-webpack-loader',
+          {
+            bypassOnDebug: true,
+            interlaced: false,
+            optimizationLevel: 7,
+          }
+        )
       ]
     },
     js: {
       test: /\.(js|jsx)$/i,
-      loader: 'babel-loader'
+      loader: query(
+        'babel-loader',
+        {
+          presets: [ 'es2015', 'react' ],
+        }
+      )
     },
     sass: {
       test: /\.scss$/i,
       loaders: [
         'style-loader',
         'css-loader',
-        'resolve-url-loader?root=' + path.join(BASE, 'src'),
-        'sass-loader?sourceMap'
+        query(
+          'resolve-url-loader',
+          {
+            root: path.join(BASE, 'src'),
+          }
+        ),
+        query(
+          'sass-loader',
+          {
+            sourceMap: true,
+          }
+        )
       ]
     }
   },
   output: {
     app: {
       filename: 'bundle.app.js',
-      path: path.join(BASE, 'app')
+      path: path.join(BASE, 'app'),
     },
     dev: {
       filename: 'bundle.dev.js',
-      path: path.join(BASE, 'dev')
+      path: path.join(BASE, 'dev'),
     }
   },
   resolve: {
     root: path.join(BASE, 'src'),
-    extensions: [ '', '.js', '.jsx' ]
+    extensions: [ '', '.js', '.jsx' ],
   },
-  target: 'web'
+  target: 'web',
 }
